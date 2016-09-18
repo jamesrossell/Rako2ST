2fc01483ef0cb72afae614920a69e8d51266810b
jamesrossell <pi@raspberrypi.(none)>
2016-09-18 15:26:11 +0100

    Made XML Parse Work

--- RakoXMLParse.py
+++ RakoXMLParse.py
@@ -1,2 +1,21 @@
-import xml.etree.ElementTree
-e = xml.etree.ElementTree.parse('rako.xml').getroot()
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+rako = ET.ElementTree(file='rako.xml')
+
+rooms = rako.findall('rooms/Room')
+print('Room count:', len(rooms))
+
+for room in rooms:
+    print('Room', room.find('Title').text)
+    print(' ID', room.get("id"))
+    print(' Type', room.find('Type').text)
+    channels = room.findall('Channel')
+    print(' Channels:', len(channels))
+    for channel in channels:        
+        print(' ID', channel.get("id"))
+        print(' Name', channel.find('Name').text)
+
+
