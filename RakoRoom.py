4d5ec1296d09efe64b0ff479e0e05fdda8c75602
James Rossell <james@wirehive.net>
2016-09-19 08:46:40 +0100

    Updates

---
+++ RakoRoom.py
@@ -0,0 +1,13 @@
+class RakoRoom:
+    """docstring for RakoRoom."""
+    roomCount = 0
+    def __init__(self, id, name):
+        super(RakoRoom, self).__init__()
+        self.id = id
+        self.name = name
+        self.scene = 0
+
+        RakoRoom.roomCount +=1
+
+    def setScene(arg):
+        pass
--- RakoXMLParse.py
+++ RakoXMLParse.py
@@ -2,20 +2,37 @@ try:
     import xml.etree.cElementTree as ET
 except ImportError:
     import xml.etree.ElementTree as ET
+class RakoRoom:
+    """docstring for RakoRoom."""
+    roomCount = 0
+    def __init__(self, id, name):
+        super(RakoRoom, self).__init__()
+        self.id = id
+        self.name = name
+        self.scene = 0
+
+        RakoRoom.roomCount +=1
+
+    def setScene(arg):
+        pass
 
 rako = ET.ElementTree(file='rako.xml')
+
+RakoRooms = []
 
 rooms = rako.findall('rooms/Room')
 print('Room count:', len(rooms))
 
 for room in rooms:
-    print('Room', room.find('Title').text)
-    print(' ID', room.get("id"))
-    print(' Type', room.find('Type').text)
+    roomTitle=('Room', room.find('Title').text)
+    roomID=(' ID', room.get("id"))
+    roomType=(' Type', room.find('Type').text)
     channels = room.findall('Channel')
-    print(' Channels:', len(channels))
-    for channel in channels:        
-        print(' ID', channel.get("id"))
-        print(' Name', channel.find('Name').text)
+    roomChannelsNum=(' Channels:', len(channels))
+    for channel in channels:
+        channelID=(' ID', channel.get("id"))
+        channelName=(' Name', channel.find('Name').text)
+    RakoRooms.append(RakoRoom(roomID,roomTitle))
 
-
+for obj in RakoRooms:
+    print (obj.name)
